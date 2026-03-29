89ee4376016aba0e4c293c0e100f4b8f5ed77005
NunezLagos <asistente.nunez@gmail.com>
2026-03-29 12:57:36 -0300

    fix: lazygit falla con oil:// path, extraer path real

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -8,7 +8,20 @@ return {
     "LazyGitFilterCurrentFile",
   },
   keys = {
-    { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "Open LazyGit" },
+    {
+      "<leader>gg",
+      function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        -- Si estamos en Oil, extraer el path real (oil:///ruta/ → /ruta/)
+        local real_path = bufname:match("^oil://(.+)") or vim.fn.expand("%:p:h")
+        if vim.fn.isdirectory(real_path) == 1 then
+          vim.cmd("LazyGit " .. vim.fn.fnameescape(real_path))
+        else
+          vim.cmd("LazyGitCurrentFile")
+        end
+      end,
+      desc = "Open LazyGit",
+    },
   },
   opts = {
     floating_window = true,
